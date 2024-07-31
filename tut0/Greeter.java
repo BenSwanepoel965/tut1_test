ec7faa794abdd7ffbf501206152323575e1cd34b
Benjamin William Swanepoel <26187396@sun.ac.za>
2024-07-31 23:17:51 +0200

    [#4]

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,5 +1,11 @@
-public class Greeter {
-	public static void main(String args[]) {
+public class Greeter { 
+	public static void main(String[] args) { 
+		greeter(args[0]); 
 	}
+
+	public static void greeter(String name) { 
+		System.out.println("Hello, " + name + "!"); 
+	}
+ 
 }
 
